c096b0b33d9e00af58601fb64c53ddaf1ced3f55
Jenny Waterfield <21JWaterfield@heckgrammar.co.uk>
2024-11-20 14:47:28 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    internal class Program
+    {
+        struct towns
+        {
+            public int population;
+            public int bathrooms;
+            public string town;
+            public string street;
+            public bool detatched;
+        }
+
+        static void Main(string[] args)
+        {
+            house h;
+            h.bedrooms = 4;
+            h.bathrooms = 4;
+            h.town = "scholes";
+            h.street = "new road east";
+            h.detatched = true;
+
+        }
+    }
+}
